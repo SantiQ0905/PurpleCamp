11beae83587824ec199851fdc482a7c912c62e9c
Santiago Quintana <59663686+SantiQ0905@users.noreply.github.com>
2023-07-26 09:51:31 -0600

    Example added

---
+++ Example.py
@@ -0,0 +1,11 @@
+#Comentario
+
+print("Lo que queramos imprimir en la consola lo tenemos que poner entre comillas.")
+
+variables = input("Las variables llevan un igual después de su nombre.")
+
+if variables == "Los ifs tienen que llevar un formato condicional, si esto, luego esto si no esto otro":
+    print("Dentro de el if tenemos que poner el codigo que deseamos realizar si se hace la condición")
+else:
+    print("De lo contrario ponemos la otra opción que queremos que se ejecute.")
+
