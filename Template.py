d780563eb994721bc47ce4ff11f801ce817fa314
Santiago Quintana <59663686+SantiQ0905@users.noreply.github.com>
2023-07-25 20:10:59 -0600

    Added instructions and template

---
+++ Template.py
@@ -0,0 +1,27 @@
+"""
+--- oOo INSTRUCCIÓNES oOo ---
+
+En la actividad de PYTHON de hoy su objetivo será hacer un "CHATBOT". Que cumpla con las siguientes caracteristicas:
+DE CONTENIDO:
+ - Tiene que tener una introducción donde se pregunte:
+    + Nombre
+    + Edad
+ - Tiene que preguntar al usuario que preferirian hacer.
+ - Tiene que preguntar al menos 3 cosas de conversación al usuario.
+ - Tiene que tener un apartado de calculadora donde al menos podamos sumar y restar numeros enteros. 
+
+DE CREATIVIDAD:
+ - Tiene que tener un dibujo de introducción.
+ - Tiene que contar con palabras de emoción (EJ. "WOW", "EPICO", "FANTASTICO", etc.).
+ - Puede contar con comentarios.
+
+DE CODIGO:
+ - Su chatbot tiene que usar PRINTS, INPUTS, IFs y ELIFs. 
+ - Tienen que cuidar la indentación, las variables y los inputs y outputs. 
+ 
+"""
+
+
+#AQUÍ EMPIEZEN SU CODIGO.
+
+print("Hello World!")
